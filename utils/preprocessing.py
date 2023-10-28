a53ee12a61e2366930d4c85c0db25d1f12a781c5
khalifardy <khalifardy.miqdarsah@gmail.com>
2023-10-28 12:23:48 +0700

    EDA

---
+++ utils/preprocessing.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Scalling:
+    def __init__(self, data):
+        self.data = data.copy()
+
+    def normalisasi(self, kolom):
+        for kol in kolom:
+            self.data[kol] = (self.data[kol]-self.data[kol].min()) / \
+                (self.data[kol].max()-self.data[kol].min())
+
+        return self.data
+
+    def standarisasi(self, kolom):
+        for kol in kolom:
+            self.data[kol] = (self.data[kol]-self.data[kol].mean()) / \
+                (self.data[kol].std())
+
+        return self.data
+
+    def log_transform(self, kolom):
+        for kol in kolom:
+            self.data[kol] = np.log(self.data[kol])
+
+        return self.data
